57b73ac8bf31ed67df3d929d403bcf0b568cf491
devCJH <http-@naver.com>
2024-07-08 15:38:15 +0900

    modal, popUp

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -2,9 +2,6 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.example.demo.vo.ResultData;
 
 @Controller
 public class UsrHomeController {
@@ -19,9 +16,8 @@ public class UsrHomeController {
 		return "redirect:/usr/home/main";
 	}
 	
-	@GetMapping("/usr/home/test")
-	@ResponseBody
-	public ResultData<String> test(String key1, String key2) {
-		return ResultData.from("코드", key1 + key2);
+	@GetMapping("/usr/home/popUp")
+	public String popUp() {
+		return "usr/home/popUp";
 	}
 }
